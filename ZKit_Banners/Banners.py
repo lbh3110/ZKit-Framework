9269fe084a011202189d50755325fb8079e7fced
Zer0 <65390948+000Zer000@users.noreply.github.com>
2020-05-23 23:57:59 +0430

    Added Linux to banner

--- ZKit_Banners/Banners.py
+++ ZKit_Banners/Banners.py
@@ -13,7 +13,7 @@ Banner1 = """
                         {--!Github https://github.com/000Zer000/ZKit-Framework!--}
                                  {--{Trojan}{KeyLogger}{Dos}{Ransomware}--}
                                      {--!@#$%? Coded By Zer0 ?%$#@!--}
-                                      {--Windows Pre-Release 0.9.1--}
+                                      {--Windows/Linux Pre-Release 0.9.1--}
 
 """ 
 Banner2 = 'Not Builded Right Now'
